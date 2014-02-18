76db3c49c2db8795bc0ed7552d78e4a8376db253
maxxas <maxschamber@gmail.com>
2014-02-18 22:41:10 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tshirtshop::Application.config.secret_key_base = '2e4b068c575d2f99f350dd3232a62b568a6473ab2e7d32ed50a950893f935940e2463fa4462e0a5746353d799b112a4f59dbb1299da80f6ddf98241dca20595f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tshirtshop::Application.config.session_store :cookie_store, key: '_tshirtshop_session'
